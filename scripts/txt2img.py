d8b7aff637f507ef227cf5c8722bd50e3e2de84a
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-22 21:18:10 +0800

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -14,7 +14,7 @@ from ldm.models.diffusion.plms import PLMSSampler
 
 def load_model_from_config(config, ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
+    pl_sd = torch.load(ckpt, map_location="cuda")
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
